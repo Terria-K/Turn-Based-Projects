62d955d94a03acd285081a7f44e2ad1bd538093f
Terria028 <86240437+Terria028@users.noreply.github.com>
2022-10-06 18:48:23 +0800

    Added teal and simplified entity.lua

--- lua/entity.lua
+++ lua/entity.lua
@@ -1,15 +1,20 @@
-local new = function(name)
-	Entity = { name = name, hp = 100 }
-	function Entity:deal_damage_to(entity, damage)
-		entity.hp = entity.hp - damage
-		print(string.format("%s Damages => %s with %d dmg", self.name, entity.name, damage))
-		print("===============================");
-	end
+local Entity = {}
 
-	function Entity:assign_attack_damage(max)
-		return math.random(max)
-	end
-	return Entity
+function Entity.new(name)
+    local self = setmetatable({}, { __index = Entity })
+    self.name = name
+    self.hp = 100
+    return self
 end
 
-return new
+function Entity:deal_damage_to(entity, damage)
+    entity.hp = entity.hp - damage
+    print(string.format("%s Damages => %s with %d dmg", self.name, entity.name, damage));
+    print("===============================")
+end
+
+function Entity:assign_attack_damage(max)
+    return math.random(max)
+end
+
+return Entity
